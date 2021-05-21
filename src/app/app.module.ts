3dfaf32b84fc2c839a0fbd29457d3f16d3ff283c
naoyuki-iot <naoyuki1998513@gmail.com>
2021-05-21 23:47:30 +0900

    add coffeeform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SimpleFormComponent } from './simple-form/simple-form.component';
 import { FormsModule } from '@angular/forms';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
-import { BetterFormComponent } from './better-form/better-form.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpleFormComponent,
-    BetterFormComponent
+    ControlsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/controls/controls.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-controls',
+  templateUrl: './controls.component.html',
+  styleUrls: ['./controls.component.css']
+})
+export class ControlsComponent implements OnInit {
+  coffeeForm: FormGroup;
+  hotcoldsel = [ "Hot", "Cold" ];
+  constructor(private fb: FormBuilder) { 
+    this.coffeeForm = this.fb.group({
+      name: "ブレンド",
+      taste: "バランスのよい口当たり",
+      hotcold: this.hotcoldsel[0],
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
